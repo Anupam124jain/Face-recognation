026a2f8cba93af19841671c77ca1fab5d7d26e63
Anupam  Jain <anupam124jain@gmail.com>
2021-11-23 19:18:24 +0530

    add changes icon detection

--- icon_tag_extraction_poc/croping_tag_icon.py
+++ icon_tag_extraction_poc/croping_tag_icon.py
@@ -7,10 +7,19 @@ h, w, c = img.shape
 
 # Chip icon
 x = round(0.081198275 * w)
+print("-----------x-------------")
+print(x)
+print("----------------x--------------")
 y = round(0.27992284 * h)
 w = round(0.19849685 * w)
 h = round(0.28704292 * h)
+print("-----------h-------------")
+print(h)
+print("----------------h--------------")
 
 crop_img = img[y:y+h, x:x+w]
+print("---------crop_img-----------")
+print(crop_img)
+print("---------crop_img-----------")
 
 cv2.imwrite("cropped_chip_icon.jpg", crop_img)
